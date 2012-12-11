793097bd694469524fdbcfed5a1e3dbee03d552d
Bob Burbach <bob.burbach@gmail.com>
2012-12-11 10:34:03 -0800

    Add ability to define format and formatters on each field that you want parsed. Notice changes to the json file format (is now an array of objects). Convert fixed width ruby class from class methods to instance methods and use attr_accessor so that we don't have to pass as many arguments around.

--- fixed_width_to_csv.rb
+++ fixed_width_to_csv.rb
@@ -3,35 +3,82 @@
 require 'json'
 
 class FixedWidthToCSV
-  def self.parse(data_file, data_definition_file, output_file_name)
-    puts "reading definition file..."
-    data_definition = JSON.parse( File.read(data_definition_file) )
+  attr_accessor :data_file, :data_definition, :output_file_name, :formatters, :values_needing_format
 
+  def initialize(data_file, data_definition_file, output_file_name)
+    @data_file = data_file
+
+    puts "reading definition file..."
+    @data_definition = JSON.parse( File.read(data_definition_file) )
+
+    @output_file_name = output_file_name
+
+    puts "determining values needing format"
+    @values_needing_format = @data_definition.map{|d| d.length > 1 ? [d, @data_definition.index(d)] : nil}.compact
+
+    @formatters = Formatters.new
+    
+    parse
+  end
+
+  def parse
     puts "creating output file for writing..."
-    output_file = File.open(output_file_name, 'w')
+    @output_file = File.open(@output_file_name, 'w')
 
     puts "generating headers..."
-    output_file.puts( generate_headers(data_definition) )
-    
+    @output_file.puts( generate_headers )
+
     puts "writing rows..."
-    file = File.open(data_file, 'r')
+    file = File.open(@data_file, 'r')
     while !file.eof?
-       row = file.readline.strip
-       next if row == ""
-       output_file.puts( generate_row(data_definition, row) )
+      row = file.readline.strip
+      next if row == ""
+
+      row = generate_row(row)
+      row = format_row(row)
+      @output_file.puts( row.join(',') )
     end
 
     puts "closing output file..."
-    output_file.close
+    @output_file.close
   end
   
-  def self.generate_headers(data_definition)
-    data_definition.keys.join(',')
+  def generate_headers
+    @data_definition.map{|d| d.keys.first}.flatten.join(',')
   end
 
-  def self.generate_row(data_definition, row)
-    row.unpack(data_definition.values.map{|v| "A#{v}"}.join).join(',')
+  def generate_row(row)
+    values = @data_definition.map{|d| d.values.first}
+    row.unpack(values.map{|v| "A#{v}"}.join)
+  end
+
+  def format_row(row)
+    @values_needing_format.each do |data, index|
+      current_format = data["format"]
+      new_format = data["convert_to"]
+      value = row[index]
+     
+      new_value = @formatters.send("#{current_format}_to_#{new_format}", value)
+
+      row[index] = new_value
+    end
+    row
   end
 end
 
-FixedWidthToCSV.parse(ARGV[0], ARGV[1], ARGV[2])
+class Formatters
+  def mddyy_to_mmddyy(value)
+    value = value.strip
+    if value.length == 6
+      value
+    else
+      "0#{value}"
+    end
+  end
+
+  def mmyy_to_mmddyy(value)
+    "#{value[0..1]}01#{value[2..3]}"
+  end
+end
+
+FixedWidthToCSV.new(ARGV[0], ARGV[1], ARGV[2]).parse
